d1cf15312a134f963558aff2b5b5fee1ddf32e6d
Hector M <moralh2@gmail.com>
2019-01-06 23:17:21 -0600

    exit after success

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,20 +6,20 @@ var mysql = require("mysql")
 var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: keys.db.pw,
-  database: "bamazon_DB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.db.pw,
+    database: "bamazon_DB"
 });
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     // afterConnection();
-    queryProductLine();
-  });
-  
+    buyProduct();
+});
+
 //   function afterConnection() {
 //     connection.query(`SELECT * FROM products`, function(err, res) {
 //       if (err) throw err;
@@ -28,72 +28,74 @@ var connection = mysql.createConnection({
 //     });
 //   }
 
-  function queryProductLine() {
+function buyProduct() {
     // query the database for all products
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      // collect list, prompt user to select one
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].name);
-              }
-              return choiceArray;
-            },
-            message: "Which item would you like to purchase?"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to buy?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].name === answer.choice) {
-              chosenItem = results[i];
-            }
-          }
-
-          var userQty = parseInt(answer.quantity)
-
-          console.log(chosenItem)
-          console.log(chosenItem.quantity)
-          console.log(userQty)
-
-          // determine if enough in stock
-          if (chosenItem.quantity >= userQty) {
-            var newQuantity = chosenItem.quantity - userQty
-            // bid was high enough, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        // collect list, prompt user to select one
+        inquirer
+            .prompt([
                 {
-                  quantity: newQuantity
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which item would you like to purchase?"
                 },
                 {
-                  id: chosenItem.id
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to buy? (integer)",
+                    validate: function (value) {
+                        if ((isNaN(value) === false) && (Number(value) === parseInt(value))) {
+                            return true;
+                        }
+                        return false;
+                    }
                 }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("Your purchase was successful!");
-                // start();
-              }
-            );
-          }
-          else {
-            // if not enough in stock
-            console.log("We don't have enough in stock...");
-            // start();
-          }
-        });
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+                var userQty = parseInt(answer.quantity)
+                // determine if enough in stock
+                if (chosenItem.quantity >= userQty) {
+                    var newQuantity = chosenItem.quantity - userQty
+                    // bid was high enough, so update db, let the user know, and start over
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                quantity: newQuantity
+                            },
+                            {
+                                id: chosenItem.id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Your purchase was successful!");
+                            connection.end();
+                            // start();
+                        }
+                        
+                    );
+                }
+                else {
+                    // if not enough in stock
+                    console.log("We don't have enough in stock...");
+                    // start();
+                }
+            });
     });
-  }
+}
